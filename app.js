7f12494c9866b3db64336be3228d5c29e1a9ed80
np1010 <72358183+np1010@users.noreply.github.com>
2020-10-06 02:13:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog",blogSchema);
 // Blog.create({
-// title: "this is campground",
+// title: "this is blog title",
 // image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-// body: "wala wala campground main wala"
+// body: "wala wala"
 // });
 //routes
 app.get("/",function(req , res){
